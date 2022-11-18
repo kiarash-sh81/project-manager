0d7ebf184e36717cdc0d243b82a3259c112bf8d7
kiarash-sh81 <kiarashshahroudi@gmail.com>
2022-11-18 07:45:15 +0330

    debug creat project

--- app/http/validation/project.js
+++ app/http/validation/project.js
@@ -1,8 +1,10 @@
 const { body } = require("express-validator");
 
 function createProjectValidation(){
-  body("title").notEmpty().withMessage("title should not be empty"),
-  body("text").notEmpty().isLength({min: 25}).withMessage("text should be at least 25 char and should not be empty")  
+  return [
+    body("title").notEmpty().withMessage("title should not be empty"),
+    body("text").notEmpty().isLength({min: 25}).withMessage("text should be at least 25 char and should not be empty")  
+  ]
 }
 
 module.exports = {
