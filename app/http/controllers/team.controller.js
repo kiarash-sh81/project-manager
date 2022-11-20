28829358c003c79fadb3d0aacd54285e4791db51
kiarash-sh81 <kiarashshahroudi@gmail.com>
2022-11-20 13:47:07 +0330

    getAllTeam and getMyTeam and removeTeam done

--- app/http/controllers/team.controller.js
+++ app/http/controllers/team.controller.js
@@ -2,26 +2,90 @@ const { teamModel } = require("../../models/team");
 
 class teamController{
     async creatTeam(req ,res, next){
-        const {name, username, description} = req.body;
-        const owner = req.user._id;
-        const creating = await teamModel.create({
-            name,
-            username,
-            description,
-            owner
-        });
-        if(!creating) throw {status:500,success: false , message: "cant creating team"};
-        return res.status(201).json({
-            status:201,
-            success:true,
-            message: "team created successfully"
-        })
+        try {
+            const {name, username, description} = req.body;
+            const owner = req.user._id;
+            const creating = await teamModel.create({
+                name,
+                username,
+                description,
+                owner
+            });
+            if(!creating) throw {status:500,success: false , message: "cant creating team"};
+            return res.status(201).json({
+                status:201,
+                success:true,
+                message: "team created successfully"
+            })
+            
+        } catch (error) {
+            next(error)
+        }
+    }
+    async getAllTeam(req, res, next){
+        try {
+            const teams =await teamModel.find({});
+            if(!teams) throw {status:500 , success: false , message:"cant finding team"};
+            return res.status(200).json({
+                status:200,
+                success: true,
+                teams
+            });
+        } catch (error) {
+            next(error)
+        }
+    }
+    async getTeamByID(req, res, next){
+        try {
+            const teamID = req.params.id;
+            const finding = await teamModel.findById(teamID);
+            if(!finding) throw {status:404 , success:false , message:"team not founded"};
+            return res.status(200).json({
+                status:200,
+                success: true,
+                finding
+            });
+        } catch (error) {
+            next(error)
+        }
+    }
+    async getMyTeam(req, res, next){
+        try {
+            const userID = req.user._id;
+            const teams =await teamModel.find({
+                $or :[
+                    {owner:userID},
+                    {users:userID}
+                ]
+            });
+            if(!teams) throw {status:500 , success: false , message:"cant finding the teams"};
+            return res.status(200).json({
+                status:200,
+                success: true,
+                teams
+            });
+        } catch (error) {
+            next(error)
+        }
     }
     inviteUserToTeam(){
 
     }
-    removeTeam(){
-
+    async removeTeamByID(req, res, next){
+        try {
+            const teamID = req.params.id;
+            const finding = await teamModel.find({_id:teamID});
+            if(!finding) throw {status: 404 , success: false , message: "team not founded"};
+            const removing = await teamModel.deleteOne({_id:teamID});
+            if(removing.deletedCount == 0) throw {status:500 , success: false , message:"team cant deleted succussfully"};
+            return res.status(200).json({
+                status:200,
+                success:true,
+                message:"team deleted successfully"
+            });
+        } catch (error) {
+            next(error)
+        }
     }
     updateTeam(){
 
--- app/routers/team.js
+++ app/routers/team.js
@@ -1,11 +1,16 @@
 const {teamController} = require('../http/controllers/team.controller');
 const { checkLogin } = require('../http/middlewares/autoLogin');
 const { expressValidatorMapper } = require('../http/middlewares/checkError');
+const { mongoIDValidation } = require('../http/validation/public');
 const { creatingTeamValidation } = require('../http/validation/team');
 
 const router = require('express').Router();
 
 router.post("/create" , checkLogin  ,creatingTeamValidation() , expressValidatorMapper, teamController.creatTeam);
+router.get("/list" , checkLogin , teamController.getAllTeam);
+router.get("/me" , checkLogin , teamController.getMyTeam);
+router.get("/:id" , checkLogin , mongoIDValidation() , expressValidatorMapper ,teamController.getTeamByID);
+router.delete("/remove/:id" , checkLogin ,mongoIDValidation() , expressValidatorMapper , teamController.removeTeamByID);
 
 module.exports = {
     teamRouter : router
